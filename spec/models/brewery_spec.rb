bc107673b9a04d0c4a9f66ea15a9138b0cd0991e
Anni <nadeka@users.noreply.github.com>
2016-02-12 20:32:25 +0200

    Add specs for brewery model

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Brewery, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "is not saved without a name" do
+        brewery = Brewery.create year:1927
+
+        expect(brewery).to_not be_valid
+        expect(Brewery.count).to eq(0)
+    end
+
+    it "is not saved without a year" do
+        brewery = Brewery.create name:"brewery"
+
+        expect(brewery).to_not be_valid
+        expect(Brewery.count).to eq(0)
+    end
+
+    describe "with a name and a year" do
+        let(:brewery){ Brewery.create name:"brewery", year:1927 }
+        let!(:beer){ Beer.create name:"beer", style:"lager", brewery_id:brewery.id }
+
+        it "is saved" do
+            expect(brewery.name).to eq("brewery")
+            expect(brewery.year).to eq(1927)
+
+            expect(brewery).to be_valid
+            expect(Brewery.count).to eq(1)
+        end
+
+        it "and with two ratings, has the correct average rating" do
+            Rating.create score:10, beer_id:beer.id
+            Rating.create score:20, beer_id:beer.id
+
+            expect(brewery.ratings.count).to eq(2)
+            expect(brewery.avg_rating).to eq(15.0)
+        end
+    end
 end
