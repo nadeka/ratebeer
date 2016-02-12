9bc9c621c54becad3570c02553cabaec8ad4187f
Anni <nadeka@users.noreply.github.com>
2016-02-12 23:45:07 +0200

    Round average rating, move edit and destroy links to show page

--- app/models/concerns/average_rating.rb
+++ app/models/concerns/average_rating.rb
@@ -3,6 +3,6 @@ module AverageRating
 
     def avg_rating
         return 0 if ratings.empty?
-        ratings.inject(0) { |sum, rating| sum + rating.score } / ratings.count.to_f
+        (ratings.inject(0) { |sum, rating| sum + rating.score } / ratings.count.to_f).round(1)
     end
 end
