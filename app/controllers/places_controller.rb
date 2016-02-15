3686c13e286f8f3531ba0560ebe871723ec05c31
Anni <nadeka@users.noreply.github.com>
2016-02-15 17:33:42 +0200

    Add ability to search bars

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,6 +3,23 @@ class PlacesController < ApplicationController
     end
 
     def search
-        render: index
+        api_key = "96ce1942872335547853a0bb3b0c24db"
+        url = 'http://stark-oasis-9187.herokuapp.com/api/'
+
+        response = HTTParty.get "#{url}#{ERB::Util.url_encode(params[:city])}"
+
+        places_from_api = response.parsed_response["bmp_locations"]["location"]
+
+        if places_from_api.is_a?(Hash) and places_from_api['id'].nil?
+            redirect_to places_path, :notice => "No places in #{params[:city]}."
+        else
+            places_from_api = [places_from_api] if places_from_api.is_a?(Hash)
+
+            @places = places_from_api.map do | location |
+                Place.new(location)
+            end
+
+            render :index
+        end
     end
 end
